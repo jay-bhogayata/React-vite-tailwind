d2a2297f0cce404c5e3d752332702a7d9d7d81bd
Jay Bhogayata <jaybhogayata53@gmail.com>
2022-07-17 21:21:58 +0530

    basic setup is ready

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App grid h-screen place-items-center">
+      <h1
+      className='text-3xl text-center mx-auto my-auto text-gray-300'
+      >React + vite + tailwind 🔥🔥🔥</h1>
+    </div>
+  )
+}
+
+export default App
